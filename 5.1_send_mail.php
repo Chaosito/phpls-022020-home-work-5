bb4530c3f2fc14b5574e5c8dd833ba1c55ef064d
Sergey Aleinikov <chaositomail@gmail.com>
2020-03-08 11:50:27 +0600

    Few fixes

--- 5.1_send_mail.php
+++ 5.1_send_mail.php
@@ -7,7 +7,7 @@ const MAIL_PORT         = 465;
 const MAIL_USER         = '***USER***';
 const MAIL_PASS         = '***PASSWORD***';
 const MAIL_ENCRYPTION   = 'SSL';
-const MAIL_FROM_NAME    = 'Burgers';
+const MAIL_FROM_NAME    = 'User';
     
 try {
     $transport = (new Swift_SmtpTransport(MAIL_SERVER, MAIL_PORT))
@@ -15,17 +15,18 @@ try {
         ->setPassword(MAIL_PASS)
         ->setEncryption(MAIL_ENCRYPTION);
 
-// Create the Mailer using your created Transport
     $mailer = new Swift_Mailer($transport);
 
-// Create a message
     $message = (new Swift_Message("MailSubject"))
         ->setFrom([MAIL_USER => MAIL_FROM_NAME])
         ->setTo([MAIL_USER])
         ->setBody('mail text');
 
-// Send the message
     $result = $mailer->send($message);
+    
+    if ($result) {
+        print "Mail sended!";
+    }
 } catch (Exception $e) {
     die('Error: '.$e->getMessage());
 }
